cbb65bf0cf1673ceec32f46f2cff0d6838301c4e
Jishnu Raj T <jriosdev@gmail.com>
2018-07-10 12:25:32 +0530

    init

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,15 +1,31 @@
 import Vapor
 
 /// Register your application's routes here.
+//public func routes(_ router: Router) throws {
+//    // Basic "Hello, world!" example
+//    router.get("hello") { req in
+//        return "Hello, world!"
+//    }
+//
+//    // Example of configuring a controller
+//    let todoController = TodoController()
+//    router.get("todos", use: todoController.index)
+//    router.post("todos", use: todoController.create)
+//    router.delete("todos", Todo.parameter, use: todoController.delete)
+//}
 public func routes(_ router: Router) throws {
-    // Basic "Hello, world!" example
-    router.get("hello") { req in
-        return "Hello, world!"
+    router.get("name") { req in
+        return "Ethan Hunt"
     }
-
-    // Example of configuring a controller
-    let todoController = TodoController()
-    router.get("todos", use: todoController.index)
-    router.post("todos", use: todoController.create)
-    router.delete("todos", Todo.parameter, use: todoController.delete)
+    router.get("age") { req in
+        return "\(23)"
+    }
+    router.get("json") { req in
+        return Person(name: "Martin J. Lasek", age: 26)
+    }
+}
+// Important: your class or struct conforms to Content
+struct Person: Content {
+    var name: String
+    var age: Int
 }
